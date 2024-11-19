a76d95a7a33cc0ec14079c5af6ede5c7f7fd01dc
siemmusa <siemmusa2001@gmail.com>
2024-11-18 21:21:12 -0500

    Commit

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 def main():
     try:
@@ -7,16 +7,11 @@ def main():
         # You can draw the mole with this snippet:
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
+        mole_width, mole_height = mole_image.get_size()
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
-        running = True
-        mole_image = pygame.image.load("mole.png")
-        mole_width, mole_height = mole_image.get_size()
         mole_x, mole_y = 0, 0
-        for x in range(0, screen_width, grid_cell_size):
-                pygame.draw.line(screen, (200, 200, 200), (x, 0), (x, 512))
-            for y in range(0, screen_height, grid_cell_size):
-                pygame.draw.line(screen, (200, 200, 200), (0, y), (640, y))
+        running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -26,7 +21,12 @@ def main():
                     if (mole_x <= mouse_x < mole_x + 32 and mole_y <= mouse_y < mole_y + 32):
                         mole_x = random.randrange(0, 20) * 32
                         mole_y = random.randrange(0, 16) * 32
-            screen.fill((213, 247, 242))
+            screen.fill("light green")
+            for x in range(0, 640, 32):
+                pygame.draw.line(screen, (200, 200, 200), (x, 0), (x, 512))
+            for y in range(0, 512, 32):
+                pygame.draw.line(screen, (200, 200, 200), (0, y), (640, y))
+            screen.blit(mole_image, (mole_x, mole_y))
             pygame.display.flip()
             clock.tick(60)
     finally:
